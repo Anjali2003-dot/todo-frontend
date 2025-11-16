d52d8f3979b5eba2ae561dd3ee60b5c69f9b137c
Anjali Sharma <anjal@example.com>
2025-11-16 17:57:14 +0530

    update login api url

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,7 +11,7 @@ export default function Login(){
         console.log("SUBMIT CLICKED");
 
         try {
-            const res = await fetch("http://localhost:5000/api/auth/login", {
+            const res = await fetch("https://todo-backend-4xu0.onrender.com/api/auth/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ email, password}),
